cdf1223a1496671be39d9dfc22f80d4474295d5d
Joaquim Gamero <gb.joaquim@gmail.com>
2020-05-29 13:00:16 +0200

    Finished item layout and styles

--- src/app/shared/items.service.ts
+++ src/app/shared/items.service.ts
@@ -14,38 +14,6 @@ import { Item } from './item';
   providedIn: 'root',
 })
 export class ItemsService {
-  // MOCK
-  // mock: Array<Item> = [
-  //   {
-  //     title: 'iPhone 6S Oro',
-  //     description:
-  //       'Vendo un iPhone 6 S color Oro nuevo y sin estrenar. Me han dado uno en el trabajo y no necesito el que me compré. En tienda lo encuentras por 749 euros y yo lo vendo por 740. Las descripciones las puedes encontrar en la web de apple. Esta libre.',
-  //     price: 740,
-  //     email: 'iphonemail@wallapop.com',
-  //     image:
-  //       'https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/iphone.png',
-  //   },
-  //   {
-  //     title: 'iPhone 6S Oro',
-  //     description:
-  //       'Vendo un iPhone 6 S color Oro nuevo y sin estrenar. Me han dado uno en el trabajo y no necesito el que me compré. En tienda lo encuentras por 749 euros y yo lo vendo por 740. Las descripciones las puedes encontrar en la web de apple. Esta libre.',
-  //     price: 740,
-  //     email: 'iphonemail@wallapop.com',
-  //     image:
-  //       'https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/iphone.png',
-  //   },
-  //   {
-  //     title: 'iPhone 6S Oro',
-  //     description:
-  //       'Vendo un iPhone 6 S color Oro nuevo y sin estrenar. Me han dado uno en el trabajo y no necesito el que me compré. En tienda lo encuentras por 749 euros y yo lo vendo por 740. Las descripciones las puedes encontrar en la web de apple. Esta libre.',
-  //     price: 740,
-  //     email: 'iphonemail@wallapop.com',
-  //     image:
-  //       'https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/iphone.png',
-  //   },
-  // ];
-  // items$ = of(this.mock);
-
   private itemsUrl =
     'https://frontend-tech-test-data.s3.eu-west-1.amazonaws.com/items.json';
 
